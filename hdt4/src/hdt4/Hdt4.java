9f5c5b26e251262f4e8bca4bd901906fe6bea71d
jho1213gt <jho1213gt@gmail.com>
2014-08-01 07:28:16 -0600

    Update 1.0.1

--- hdt4/src/hdt4/Hdt4.java
+++ hdt4/src/hdt4/Hdt4.java
@@ -16,7 +16,6 @@ public class Hdt4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
